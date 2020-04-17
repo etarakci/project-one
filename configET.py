345684633758d3644dc68d7f5d62ff393a35f2c8
Erendz Tarakci <etarakci@berkeley.edu>
2020-04-16 20:05:40 -0700

    started combining notebooks

--- configET.py
+++
@@ -1,7 +0,0 @@
-#  Add your OMDBAPI API key
-api_key = "c0eae06a"
-nyt_api_key = "gT2FSvOhH2TVWDw7IqVWSdCdKxbLuqnV"
-gkey = "AIzaSyBxogK0RjignvY-Uvl9DdViJrN8ujPsNIQ" #google maps api key
-#AIzaSyBmp3fp75sYxtzM1gNV6vcuTAtg3k-m9wo another key??
-census_api_key = "a08a94509f4a6722b560007c258218ee2d698ce2" 
-weather_api = "545ad856bb29ece720015452ab834945"
